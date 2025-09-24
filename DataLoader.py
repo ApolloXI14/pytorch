d3094215a025dc4419dbefbada350e57b6a77a63
Sean McHugh <samchugh@proton.me>
2025-09-24 16:54:47 -0400

    Added "DataLoader.py"

---
+++ DataLoader.py
@@ -0,0 +1,17 @@
+from torch import DataLoader
+
+def generate_batches(dataset, batch_size, shuffle=True,
+drop_last=True, device="cpu"):
+ """
+ A generator function which wraps the PyTorch DataLoader,
+ ensuring each tensor is on the write device location
+ """
+ dataloader = DataLoader(dataset=dataset,
+ batch_size=batch_size, shuffle=shuffle,
+ drop_last=drop_last)
+
+ for data_dict in dataloader:
+  out_data_dict = {};
+  for name, tensor in data_dict.items():
+   out_data_dict[name] = data_dict[name].to(device);
+  yield out_data_dict;
