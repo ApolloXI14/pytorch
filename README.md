991ce2a42e8da4f88f7b8e8318530541ea02ca40
Sean McHugh <samchugh@proton.me>
2026-01-03 20:08:30 -0500

    - Updated tokenizeTxt to take commandline args with argparse so files   can be passed to it - Updated tokenizeTxt to handle CSVs - Updated README

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Natural Language Processing with PyTorch
 
-The purpose of this repository is to build my own natural
+The purpose of this repository is to build a natural
 language processor and recurrent neural network (RNN) that can receive
 text as input and generate text as output.
+
+The ultimate goal is to vectorize text ("Vectorize.py") through the
+following steps.
+
+1. Tokenize a text document and return a word dictionary
+   (tokenizeTxt.py)
+2. Wrap the returned word dictionary in a "Vocabulary" class (Vocabulary.py), which represents the entire vocabulary of all tokens in a text
+3. Pass the "Vocabulary" class instance into the "TextVectorizer"
+   class ("Vectorize.py") that will accept text input through the
+   "vectorize" class method and return a one-hot vector based on the
+   vocabulary of the initial text document matched against the input.
+   E.g. A TextVectorizer class instance of a text containing "Hello world" that has the
+   word "Hello" passed to its "vectorize" class method will return a
+   vector of [1., 0]
+
+The tokenizer ("tokenizeTxt.py") is the most important file of the entire procedure. A dictionary of tokens provides the basis for Vectorizing a TextDataset for training.
+
+The key files in this procedure are "Vectorize.py" (which has two
+dependencies, "tokenizeTxt" to create a python dictionary of all
+"tokens" in a document, and "Vocabulary" which takes a python
+dictionary and wraps it into a "Vocabulary" class), "TextDataset.py"
+(Vectorize dependency) which takes a CSV file 
--- tokenizeTxt.py
+++ tokenizeTxt.py
@@ -1,14 +1,17 @@
+#TODO: Expand logic to process CSVs using pandas.DataFrame.loc
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.loc.html
 # Preliminary program to tokenize a single text file for eventual ML text encoding
 
+import argparse
 import os
 import re
 from io import open
 import tokenize
+import pandas as pd
 
 import spacy
 
-FILEPATH = "FILE_PATH_HERE"
-
+filePath = ''
 
 
 def preprocess_text(text):
@@ -17,8 +20,7 @@ def preprocess_text(text):
   text = re.sub(r"[^a-zA-Z.,!?]+", r" ", text)
   return text
 
-def main(filePath=""):
- #test = open(filePath, "r", encoding='utf-8').read().split('\n')
+def main(filepath=""):
  #test = open(filePath, "rb").read()
  #for line in test:
  # print(line)
@@ -34,15 +36,27 @@ def main(filePath=""):
 
   tokens = []
   nlp = spacy.load("en_core_web_sm");
-  text = open(filePath, "r", encoding="utf-8").read().lower();
-  # TODO: Text can also be processed as bytes, which can support all
-  # languages seamlessly; explore later
-  #text = nlp(open(filePath, "rb").read().lower())
-  text = preprocess_text(text); 
-  text = nlp(text); 
-  tokens = [str(token) for token in text]
   dict = {}
-  for li, token in enumerate(tokens):
+  if ".csv" in filepath:
+   print(f'pandas functionality needs to be implemented here to read {filepath}');
+   csv_file = pd.read_csv(filepath);
+   print(csv_file);
+   csv_train = csv_file.train;
+  for file in range(0,len(csv_train)):
+   print(csv_train[file]);
+   #open(csv_train[file].strip()).read();
+   print(open(csv_train[file].strip()).read());
+  if ".txt" in filepath:
+   print('.txt detected')
+   text = open(filepath, "r", encoding="utf-8").read().lower();
+   print(text)
+   # TODO: Text can also be processed as bytes, which can support all
+   # languages seamlessly; explore later
+   #text = nlp(open(filePath, "rb").read().lower())
+   text = preprocess_text(text); 
+   text = nlp(text); 
+   tokens = [str(token) for token in text]
+   for li, token in enumerate(tokens):
     # If token is NOT in dictionary, only then add, to prevent
     # skipping IDs on keys that appear more than once
     currentDictLen = len(dict.items());
@@ -51,5 +65,11 @@ def main(filePath=""):
   return dict
 
 if __name__ ==  "__main__":
-	main(FILEPATH)
+ parser = argparse.ArgumentParser();
+ parser.add_argument('-f', '--filepath', help="Filepath of file to be read and tokenized");
+ args = parser.parse_args();
+ filepath = args.filepath;
+ #test = open(filePath, "r", encoding='utf-8').read().split('\n')
+ main(filepath)
+	#main(FILEPATH)
 
